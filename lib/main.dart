e6a8502fe1482ceab2e7b8529df2793d4d5d27ad
anurag <anurag.mnda@gmail.com>
2022-07-21 21:24:29 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('I am Anurag')),
+        title: Center(child: Text('I am Rich')),
         backgroundColor: Colors.blueGrey[900],
       ),
       backgroundColor: Colors.grey,
